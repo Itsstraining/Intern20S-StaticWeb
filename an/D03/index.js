6620db87d7fb41690826cf15e17b74ddd6e290ea
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-01-08 21:24:17 +0700

    D03 Signed-off-by: Nguyễn Bình An <nguyenbinhanltv@gmail.com>

--- an/D03/index.js
+++ an/D03/index.js
@@ -1,65 +1,95 @@
-const input = document.getElementById('input');
-const btn = document.getElementById('btn');
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-btn.onclick = function() {
-    let inputValue = input.value;
-    if(inputValue) {
-        arr.push(inputValue);
-    }
-    input.value = "";
-}
-
-let fill_maxtrix = function(arr, rows, cols) {
-    let newArr = [];
-    let count = 0;
-    for(let i = 0; i < rows; i++) {
-        newArr.push([]);
-    }
-
-    //Go to right
-    for(let j = 0; j < cols; j++) {
-        newArr[0][j] = arr[count];
-        count++;
-    }
-
-    //Go to down
-    for(let i = 1; i < rows; i++) {
-        newArr[i][cols - 1] = arr[count];
-        count++;
-        
-        //Go to left
-        if(i === rows-1) {
-            for(let j = cols-2; j >= 0; j--) {
-                newArr[i][j] = arr[count];
-                count++;
-            }
+//Create matrix
+const createMatrix = (m, n, defaultValue = 0) => {
+    let arr = [];
+    for(let i = 0; i < m; i++) {
+        arr.push([]);
+        for(let j = 0; j < n; j++) {
+            arr[i].push(defaultValue);
         }
     }
-
-    //Go to up
-    for(let i = rows-2; i > 0; i--) {
-        newArr[i][0] = arr[count];
-
-        //Go to right
-        if(i === 1) {
-            for(let j = 0; j < cols-1; j++) {
-                newArr[i][j] = arr[count];
-                count++;
-            }
-        }
-        count++;
-    }
-
-    //Go to down
-    count--;
-    for(let j = cols-2; j > 0; j--) {
-        newArr[rows-2][j] = arr[count];
-        count++;
-    }
-
-    return newArr;
+    return arr;
 }
 
-console.log(fill_maxtrix(arr, 4, 5));
+//Change direction
+const changeDirection = (vector) => {
+    const cosx = 0;
+    const sinex = 1;
+
+    const {
+        x,
+        y
+    } = vector;
+    vector.x = x * cosx - y * sinex;
+    vector.y = x * sinex + y * cosx;
+}
+
+//Check collision
+const checkIfCollision = (current, matrix, m, n, defaultValue = 0) => {
+    if(current.y == m || current.x == n) {
+        return false;
+    } else {
+        if (matrix[current.y][current.x] === defaultValue) {
+            return true;
+        }
+        return false;
+    }
+}
+
+const forward = (current, vector) => {
+    current.x += vector.x;
+    current.y += vector.y;
+}
+
+const backward = (current, vector) => {
+    current.x -= vector.x;
+    current.y -= vector.y;
+}
+
+//Draw matrix
+const fillMatrix = (m, n) => {
+    let matrix = createMatrix(m, n);
+    let vector = {
+        x: 1,
+        y: 0
+    };
+    let value = 1;
+    let current = {
+        x: 0,
+        y: 0
+    };
+
+    while(value <= m * n) {
+        if(checkIfCollision(current, matrix, m, n)) {
+            matrix[current.y][current.x] = value;
+            value++;
+        } else {
+            backward(current, vector);
+            changeDirection(vector);
+        }
+        forward(current, vector);
+    }
+
+    return matrix;
+}
+
+const firstInput = document.getElementById('first-input');
+const secondInput = document.getElementById('second-input');
+const outputMatrix = document.getElementById('output-matrix');
+
+let firstValue;
+let secoundValue;
+
+secondInput.oninput = function () {
+    firstValue = firstInput.value;
+    secondValue = secondInput.value;
+    let matrix = fillMatrix(firstValue, secondValue);
+    if(outputMatrix.innerHTML !== '') {
+        outputMatrix.innerHTML = '';
+    }
+    matrix.forEach(matrix => {
+        outputMatrix.innerHTML += `<h1>${matrix}</h1>`;
+    });
+
+    firstInput.value = '';
+    secondInput.value = '';
+}
